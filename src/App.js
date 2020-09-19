4e9cafb32804ef4361fa347c6ef322e9d1137530
Jay <janitha999@gmail.com>
2020-09-19 13:32:06 +1000

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import BranchSummary from "./BranchSummary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    alert("Construct");
+    super(props);
+    this.state = {
+      field1: "Default 1",
+      field2: "Default 2",
+    };
+  }
+
+  update = (name, e) => {
+    this.setState({ [name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input
+            type="text"
+            value={this.state.field1}
+            onChange={(e) => this.update("field1", e)}
+          />
+          {this.state.field1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          <input
+            type="text"
+            value={this.state.field2}
+            onChange={(e) => this.update("field2", e)}
+          />
+          {this.state.field2}
+        </p>
+        <table>
+          <tr>
+            <BranchSummary></BranchSummary>
+          </tr>
+        </table>
+      </form>
+    );
+  }
 }
-
 export default App;
---
+++ src/BranchSummary.js
@@ -0,0 +1,45 @@
+import { getByPlaceholderText } from "@testing-library/react";
+import React from "react";
+
+function MyComponent() {
+  const [namea, eeeee] = React.useState("hi");
+  const plans = [
+    {
+      name: "Silver500",
+      premiumInDollars: 500,
+      validityInYears: 2,
+    },
+    {
+      name: "Gold250",
+      premiumInDollars: 250,
+      validityInYears: 3,
+    },
+  ];
+  return (
+    <div>
+      <div>
+        <input value={namea} onChange={(e) => eeeee(e.target.value)} />
+        {namea}
+        <ul>
+          <li> {namea}</li>
+        </ul>
+      </div>
+      <div>
+        <p>Here's the list of plans rendered in the UI using map():</p>
+        <ul>
+          {plans.map((plan) => (
+            <div>
+              <li>
+                {plan.name} - ${plan.premiumInDollars}
+              </li>
+              <span>Validity in years: {plan.validityInYears}</span>
+              <br />
+              <br />
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default MyComponent;
